46a4da5ccaf5656adac491498d01228401413d91
Marlon Veliz <marlonveliz.nero19@gmail.com>
2021-06-22 17:12:09 -0600

    Agragando rutas privadas y publicas

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
+
 
 export const LoginScreen = ({ history }) => {
 
+    const { dispatch } = useContext(AuthContext);
+
     const handleLogin = () => {
-        history.push('/');
+        // history.push('/');
+        // history.replace('/');
+
+        const lastPath = localStorage.getItem('lastPath') || '/';
+
+        const action = {
+            type: types.login,
+            payload: {
+                name: 'Marlon',
+            }
+        }
+        dispatch( action );
+
+        history.replace(lastPath);
     }
 
     return (
         <div className="container mt-5">
-            <h1>Login Screen</h1>
+            <h1>Login</h1>
             <hr />
 
             <button
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
-    Route,
 } from 'react-router-dom';
+import { AuthContext } from '../auth/AuthContext';
+import { PublicRoute } from './PublicRoute';
+import { PrivateRoute } from './PrivateRoute';
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
 
 export const AppRouter = () => {
+
+    const { user: { logged } } = useContext(AuthContext);
+
     return (
         <Router>
             <div>
                 <Switch>
-                    <Route exact path="/login" component={ LoginScreen } />
-                    <Route path="/" component={ DashboardRoutes } />
+                    <PublicRoute
+                        exact
+                        path="/login"
+                        component={ LoginScreen }
+                        isAuthenticated={ logged }
+                    />
+
+                    <PrivateRoute
+                        path="/"
+                        isAuthenticated={ logged }
+                        component={ DashboardRoutes }
+                    />
                 </Switch>
             </div>
         </Router>
+        
     )
 }
